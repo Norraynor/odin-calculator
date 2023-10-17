c36e7af618fdf5e042ff1994cb6d2dfdbafb9f8c
norraynor <37270508+Norraynor@users.noreply.github.com>
2023-10-17 20:32:01 +0200

    Add styles and scripts, reposition html.
    Revisited styles for calculator display to make it more cenetered.
    Added scripts to operate calculator.
    Repositioned numbers to make it look more like a calculator.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,10 @@
-function add(a,b){
+let firstVar = 0;
+let operator = "";
+let secondVar = 0;
+let displayValue = "0";
+const displayText = document.querySelector('.text');
+
+function add(a, b) {
     return +a + +b;
 }
 function subtract(a, b) {
@@ -12,4 +18,26 @@ function divide(a, b) {
         return "ERROR CANNOT DIVIDE BY 0";
     }
     return +a / +b;
+}
+function operate(operator, a, b) {
+    //depending on operator call different function
+    switch (operator) {
+        case "add":
+            add(a, b);
+            break;
+        case "sub":
+            subtract(a, b);
+            break;
+        case "mul":
+            multiply(a, b);
+            break;
+        case "div":
+            divide(a, b);
+            break;
+        case "del":
+            firstVar = 0;
+            secondVar = 0;
+            operator = "";
+            break;
+		}
 }
